887cee0f772cf38919730bb5c6146433e85c90c5
Liewe Gutter <lgutter@student.codam.nl>
2019-12-11 13:53:37 +0100

    changed placeholder main so tests work

--- source_files/main.c
+++ source_files/main.c
@@ -6,21 +6,21 @@
 /*   By: ivan-tey <ivan-tey@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/12/11 11:48:36 by ivan-tey       #+#    #+#                */
-/*   Updated: 2019/12/11 12:05:55 by ivan-tey      ########   odam.nl         */
+/*   Updated: 2019/12/11 13:38:07 by lgutter       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../ft_ls.h"
+#include "ft_ls.h"
+#include <stdio.h>
 
 int			main(int argc, char **argv)
 {
 	int	i;
 
-	i = 0;
-	ft_printf("argc = %d\n", argc);
+	i = 1;
 	while (argv && i < argc)
 	{
-		ft_printf("argv[%d] = %s\n", i, argv[i]);
+		dprintf(2, "ls: %s: No such file or directory\n", argv[i]);
 		i++;
 	}
 	return (0);
