f65bc2a3327bfd4347282b3660409947b74881c9
Liewe Gutter <lgutter@student.codam.nl>
2019-12-13 16:26:47 +0100

    small changes in test script

--- end2endls.sh
+++ end2endls.sh
@@ -26,7 +26,7 @@ for test in "${TESTS[@]}"; do
 	REAL=$(ls -1 $test 2>realerror)
 	FAKE=$(./ft_ls $test 2>fakeerror)
 	FAKEERROR=$(cat fakeerror)
-	echo "${FAKEERROR//ft_ls/ls}" > fakeerror
+	echo "${FAKEERROR//ft_ls/ls}" > fakeerror_temp
 	if [[ "$REAL" != "$FAKE" ]]; then
 		if [ -f testresults.txt ] && [[ "$failed" == "0" ]]; then
 			echo "" > testresults.txt
@@ -34,28 +34,28 @@ for test in "${TESTS[@]}"; do
 		fi
 		failed=$((failed + 1))
 		printf "}------------------------------------------------------------------------------{\n" >> testresults.txt
-		printf "Test %i input: ft_ls %s\n\nOutput of ls:\n˜˜˜\n%s\n˜˜˜\nOutput of ft_ls:\n˜˜˜\n%s\n˜˜˜\n" $i "$test" "$REAL" "$FAKE" >> testresults.txt
-		printf "\033[38;5;202mTEST %i FAILED!  \033[38;5;140mInput:\033[0;00m %s\n\033[38;5;140mdifference in output:\n\033[0;00m%s\n" $i "$test" "$(diff -du --label "ls output" --label "ft_ls output" <(echo "$REAL") <(echo "$FAKE"))"
-		if [[ -n "$(cat realerror)" || -n "$(cat fakeerror)" ]] && [[ -n "$(diff realerror fakeerror)" ]]; then
-			printf "\nError output of ls:\n˜˜˜\n%s\n˜˜˜\nError output of ft_ls:\n%s\n˜˜˜\n" "$(cat realerror)" "$(cat fakeerror)" >> testresults.txt
-			printf "\033[38;5;202mTest %i - \033[38;5;140mdifference in error:\n\033[0;00m%s\n" $i "$(diff -du --label "ls output" --label "ft_ls output" realerror fakeerror)"
+		printf "Test %3i input: ft_ls %s\n\nOutput of ls:\n~~~~~~\n%s\n~~~~~~\nOutput of ft_ls:\n~~~~~~\n%s\n~~~~~~\n" $i "$test" "$REAL" "$FAKE" >> testresults.txt
+		printf "\033[38;5;202mTEST %3i FAILED!  \033[38;5;140mInput:\033[0;00m %s\n\033[38;5;140mdifference in output:\n\033[0;00m%s\n" $i "$test" "$(diff -du --label "ls output" --label "ft_ls output" <(echo "$REAL") <(echo "$FAKE"))"
+		if [[ -n "$(cat realerror)" || -n "$(cat fakeerror_temp)" ]] && [[ -n "$(diff realerror fakeerror_temp)" ]]; then
+			printf "\nError output of ls:\n~~~~~~\n%s\n~~~~~~\nError output of ft_ls:\n%s\n~~~~~~\n" "$(cat realerror)" "$(cat fakeerror)" >> testresults.txt
+			printf "\033[38;5;202mTest %3i - \033[38;5;140mdifference in error:\n\033[0;00m%s\n" $i "$(diff -du --label "ls output" --label "ft_ls output" realerror fakeerror)"
 		fi
 	else
-		if [[ -n "$(cat realerror)" || -n "$(cat fakeerror)" ]] && [[ -n "$(diff realerror fakeerror)" ]]; then
+		if [[ -n "$(cat realerror)" || -n "$(cat fakeerror_temp)" ]] && [[ -n "$(diff realerror fakeerror_temp)" ]]; then
 			if [ -f testresults.txt ] && [[ "$failed" == "0" ]]; then
 				echo "" > testresults.txt
 				printf "\033[38;5;215mOverwriting testresults.txt\n\033[0;0;00m"
 			fi
 			failed=$((failed + 1))
 			printf "}------------------------------------------------------------------------------{\n" >> testresults.txt
-			printf "Test %i input: ft_ls %s\n\nError output of ls:\n˜˜˜\n%s\n˜˜˜\nError output of ft_ls:\n%s\n˜˜˜\n" $i "$test" "$(cat realerror)" "$(cat fakeerror)" >> testresults.txt
-			printf "\033[38;5;202mTEST %i FAILED!  \033[38;5;140mInput:\033[0;00m %s\n\033[38;5;140mdifference in error:\n\033[0;00m%s\n" $i "$test" "$(diff -du --label "ls output" --label "ft_ls output" realerror fakeerror)"
+			printf "Test %3i input: ft_ls %s\n\nError output of ls:\n~~~~~~\n%s\n~~~~~~\nError output of ft_ls:\n%s\n~~~~~~\n" $i "$test" "$(cat realerror)" "$(cat fakeerror)" >> testresults.txt
+			printf "\033[38;5;202mTEST %3i FAILED!  \033[38;5;140mInput:\033[0;00m %s\n\033[38;5;140mdifference in error:\n\033[0;00m%s\n" $i "$test" "$(diff -du --label "ls output" --label "ft_ls output" realerror fakeerror)"
 		else
-			printf "\033[38;5;140mTest %i \033[38;5;41msuccess!\033[38;5;140m Input:\033[0;00m %s\n" $i "$test"
+			printf "\033[38;5;140mTest %3i \033[38;5;41msuccess!\033[38;5;140m Input:\033[0;00m %s\n" $i "$test"
 		fi
 	fi
-	rm -f realerror fakeerror
 done
+rm -f realerror fakeerror fakeerror_temp
 if [[ "$failed" != "0" ]]; then
 	printf "\n\033[38;5;140mTests completed, \033[38;5;202mfailed %i out of %i tests.\033[38;5;140m for exact output of failed tests, see testresults.txt.\033[0;00m\n" $failed $i
 	exit 1
