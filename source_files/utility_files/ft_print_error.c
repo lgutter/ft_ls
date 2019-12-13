405095c2de7c201a074af32fb215a2cdb729ac35
Liewe Gutter <lgutter@student.codam.nl>
2019-12-13 13:43:11 +0100

    changed error messages to ft_ls

--- source_files/utility_files/ft_print_error.c
+++ source_files/utility_files/ft_print_error.c
@@ -6,7 +6,7 @@
 /*   By: lgutter <lgutter@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/12/13 10:43:57 by lgutter        #+#    #+#                */
-/*   Updated: 2019/12/13 11:34:12 by lgutter       ########   odam.nl         */
+/*   Updated: 2019/12/13 13:42:10 by lgutter       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@ void	ft_print_error(char *errdex, char *errstr)
 {
 	char *usage;
 
-	usage = "usage: ls [-ABCFGHLOPRSTUWabcdefghiklmnopqrstuwx1] [file ...]";
+	usage = "usage: ft_ls [-ABCFGHLOPRSTUWabcdefghiklmnopqrstuwx1] [file ...]";
 	if (ft_strcmp(errdex, "notfound") == 0)
 	{
-		ft_dprintf(2, "ls: %s: No such file or directory\n", errstr);
+		ft_dprintf(2, "ft_ls: %s: No such file or directory\n", errstr);
 	}
 	else if (ft_strcmp(errdex, "option") == 0)
 	{
-		ft_dprintf(2, "ls: illegal option -- %c\n", *errstr);
+		ft_dprintf(2, "ft_ls: illegal option -- %c\n", *errstr);
 	}
 	else
 	{
--- test_files/ft_print_error_tests.c
+++ test_files/ft_print_error_tests.c
@@ -6,7 +6,7 @@
 /*   By: lgutter <lgutter@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/12/13 12:24:00 by lgutter        #+#    #+#                */
-/*   Updated: 2019/12/13 12:29:01 by lgutter       ########   odam.nl         */
+/*   Updated: 2019/12/13 13:42:26 by lgutter       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,5 @@ Test(stupid, very_stupid_test_1, .init = redirect_std_err)
 	ft_print_error("usage", NULL);
 	fflush(stderr);
 
-	cr_assert_stderr_eq_str("usage: ls [-ABCFGHLOPRSTUWabcdefghiklmnopqrstuwx1] [file ...]\n");
+	cr_assert_stderr_eq_str("usage: ft_ls [-ABCFGHLOPRSTUWabcdefghiklmnopqrstuwx1] [file ...]\n");
 }
