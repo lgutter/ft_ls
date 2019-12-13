9a7a3b3f4ad4bf477c519b23240ec50eab7f16c8
Liewe Gutter <lgutter@student.codam.nl>
2019-12-13 12:01:30 +0100

    added input to failed test message

--- end2endls.sh
+++ end2endls.sh
@@ -33,7 +33,7 @@ for test in "${TESTS[@]}"; do
 		failed=$((failed + 1))
 		printf "}------------------------------------------------------------------------------{\n" >> testresults.txt
 		printf "Test %i input: ft_ls %s\n\nOutput of ls:\n˜˜˜\n%s\n˜˜˜\nOutput of ft_ls:\n˜˜˜\n%s\n˜˜˜\n" $i "$test" "$REAL" "$FAKE" >> testresults.txt
-		printf "\033[38;5;202mTEST %i FAILED!\n\033[38;5;140mdifference in output:\n\033[0;00m%s\n" $i "$(diff -du --label "ls output" --label "ft_ls output" <(echo "$REAL") <(echo "$FAKE"))"
+		printf "\033[38;5;202mTEST %i FAILED!  \033[38;5;140mInput:\033[0;00m %s\n\033[38;5;140mdifference in output:\n\033[0;00m%s\n" $i "$test" "$(diff -du --label "ls output" --label "ft_ls output" <(echo "$REAL") <(echo "$FAKE"))"
 		if [[ -n "$(cat realerror)" || -n "$(cat fakeerror)" ]] && [[ -n "$(diff realerror fakeerror)" ]]; then
 			printf "\nError output of ls:\n˜˜˜\n%s\n˜˜˜\nError output of ft_ls:\n%s\n˜˜˜\n" "$(cat realerror)" "$(cat fakeerror)" >> testresults.txt
 			printf "\033[38;5;202mTest %i - \033[38;5;140mdifference in error:\n\033[0;00m%s\n" $i "$(diff -du --label "ls output" --label "ft_ls output" realerror fakeerror)"
@@ -47,9 +47,9 @@ for test in "${TESTS[@]}"; do
 			failed=$((failed + 1))
 			printf "}------------------------------------------------------------------------------{\n" >> testresults.txt
 			printf "Test %i input: ft_ls %s\n\nError output of ls:\n˜˜˜\n%s\n˜˜˜\nError output of ft_ls:\n%s\n˜˜˜\n" $i "$test" "$(cat realerror)" "$(cat fakeerror)" >> testresults.txt
-			printf "\033[38;5;202mTEST %i FAILED!\n\033[38;5;140mdifference in error:\n\033[0;00m%s\n" $i "$(diff -du --label "ls output" --label "ft_ls output" realerror fakeerror)"
+			printf "\033[38;5;202mTEST %i FAILED!  \033[38;5;140mInput:\033[0;00m %s\n\033[38;5;140mdifference in error:\n\033[0;00m%s\n" $i "$test" "$(diff -du --label "ls output" --label "ft_ls output" realerror fakeerror)"
 		else
-			printf "\033[38;5;140mTest $i \033[38;5;41msuccess! \033[38;5;140minput:\033[0;00m $test\n"
+			printf "\033[38;5;140mTest %i \033[38;5;41msuccess!\033[38;5;140m Input:\033[0;00m %s\n" $i "$test"
 		fi
 	fi
 	rm -f realerror fakeerror
@@ -58,6 +58,6 @@ if [[ "$failed" != "0" ]]; then
 	printf "\n\033[38;5;140mTests completed, \033[38;5;202mfailed %i out of %i tests.\033[38;5;140m for exact output of failed tests, see testresults.txt.\033[0;00m\n" $failed $i
 	exit 1
 else
-	printf "\n\033[38;5;140mTests completed, \033[38;5;41mall %i tests passed!\033[0;00m\n" $i
+	printf "\n\033[38;5;140mTests completed,\033[38;5;41m all %i tests passed!\033[0;00m\n" $i
 	exit 0
 fi
