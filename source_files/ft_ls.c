26feb9b5188d91de20d92fbcf4e65fb470fe3afe
Liewe Gutter <lgutter@student.codam.nl>
2019-12-18 17:58:58 +0100

    fixed if else tree in ft_ls

--- source_files/ft_ls.c
+++ source_files/ft_ls.c
@@ -6,7 +6,7 @@
 /*   By: lgutter <lgutter@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/12/16 11:53:01 by lgutter        #+#    #+#                */
-/*   Updated: 2019/12/18 17:32:16 by lgutter       ########   odam.nl         */
+/*   Updated: 2019/12/18 17:58:06 by lgutter       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,20 @@ int		ft_ls(int filecount, char **filenames, t_options options)
 	t_file_info		*list_start;
 	t_file_info		*current;
 	int				i;
-	int				ret;
 
 	i = 0;
 	list_start = NULL;
 	while (i < filecount)
 	{
 		current = ft_stats_to_list(filenames[i], &list_start);
-		if (current == NULL)
-			ft_print_error("notfound", filenames[i]);
-		else if (S_ISLNK(current->stats.st_mode) && (options & e_opt_l) == 0)
+		if (current != NULL &&\
+			S_ISLNK(current->stats.st_mode) && (options & e_opt_l) == 0)
 		{
-			ret = stat(filenames[i], &(current->stats));
-			if (ret != 0)
+			if (stat(filenames[i], &(current->stats)) != 0)
 				return (-1);
 		}
+		if (current == NULL)
+			ft_print_error("notfound", filenames[i]);
 		else if (S_ISDIR(current->stats.st_mode))
 			ft_dir_to_list(current->path, options);
 		else
