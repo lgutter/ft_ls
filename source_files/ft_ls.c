1e1a3a2d86d67c8bc534cf67555f104823aa74ef
Liewe Gutter <lgutter@student.codam.nl>
2019-12-18 17:30:43 +0100

    added check to ft_ls for symlinks

--- source_files/ft_ls.c
+++ source_files/ft_ls.c
@@ -6,7 +6,7 @@
 /*   By: lgutter <lgutter@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/12/16 11:53:01 by lgutter        #+#    #+#                */
-/*   Updated: 2019/12/17 19:35:18 by lgutter       ########   odam.nl         */
+/*   Updated: 2019/12/18 17:19:57 by lgutter       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,19 @@ int		ft_ls(int filecount, char **filenames, t_options options)
 	t_file_info		*list_start;
 	t_file_info		*current;
 	int				i;
+	int				ret;
 
 	i = 0;
 	list_start = NULL;
-	(void)options;
 	while (i < filecount)
 	{
 		current = ft_stats_to_list(filenames[i], &list_start);
+		if (S_ISLNK(current->stats.st_mode) && (options & e_opt_l) == 0)
+		{
+			ret = stat(filenames[i], &(current->stats));
+			if (ret != 0)
+				return (-1);
+		}
 		if (current == NULL)
 			ft_print_error("notfound", filenames[i]);
 		else if (S_ISDIR(current->stats.st_mode))
