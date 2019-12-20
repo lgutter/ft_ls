8c2574d857195c6dbc207255db8fe74b497bb10e
Liewe Gutter <lgutter@student.codam.nl>
2019-12-20 16:10:30 +0100

    improved comments in header
    Comments now show up when hovering over the function or element it is place above.

--- ft_ls.h
+++ ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: ivan-tey <ivan-tey@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/12/11 11:33:09 by ivan-tey       #+#    #+#                */
-/*   Updated: 2019/12/20 13:00:47 by lgutter       ########   odam.nl         */
+/*   Updated: 2019/12/20 15:48:58 by lgutter       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,14 +28,12 @@
 /*
 **	Custom errno defines
 */
-
 # define EUSAGE 4242
 # define EILLOPT 242
 
 /*
 **	enumerations for the options
 */
-
 enum					e_options
 {
 	e_opt_l = 1 << 0,
@@ -49,20 +47,18 @@ enum					e_options
 **	typedef of an unsigned long for the options,
 **	this is where the above listen enums are stored.
 */
-
 typedef unsigned long	t_options;
 
 /*
-** struct with info of a file:
-**	stats: struct as returned by the function lstat
-**	path: complete path of the file as given as argument
-**	name_pointer: a pointer to the position in path where the filename starts
-**	u_name: name of the user as given by getpwuid
-**	g_name: name of the group as given by getgrgid
-**	link_path: the path contained in the symlink as given by readlink
-**	next: a pointer to the next element in the list of info structs
+**	struct with info of a file:
+**		stats: struct as returned by the function lstat
+**		path: complete path of the file as given as argument
+**		name_pointer: pointer to the position in path where the filename starts
+**		u_name: name of the user as given by getpwuid
+**		g_name: name of the group as given by getgrgid
+**		link_path: the path contained in the symlink as given by readlink
+**		next: a pointer to the next element in the list of info structs
 */
-
 typedef struct			s_file_info
 {
 	struct stat			stats;
@@ -77,7 +73,6 @@ typedef struct			s_file_info
 /*
 **	Core functions
 */
-
 int						main(int argc, char **argv);
 int						ft_ls(int filecount,\
 								char **filenames, t_options options);
@@ -85,14 +80,12 @@ int						ft_ls(int filecount,\
 /*
 ** Find functions
 */
-
 int						ft_find_options(int argc,\
 										char **argv, t_options *options);
 
 /*
 ** List functions
 */
-
 t_file_info				*ft_stats_to_list(char *filename,\
 										t_file_info **list_start);
 int						ft_dir_to_list(char *dirname, t_options options);
@@ -100,7 +93,6 @@ int						ft_dir_to_list(char *dirname, t_options options);
 /*
 **	Utility functions
 */
-
 char					*ft_find_name_pointer(char path[PATH_MAX + 1]);
 void					ft_print_error(int errnumber, char *errstr);
 void					*ft_error_free(void **memory);
