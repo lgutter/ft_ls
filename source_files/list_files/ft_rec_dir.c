7ff14a9ba5404949f35aab18133d402cba9a217d
Imre van Teylingen <ivan-tey@student.codam.nl>
2019-12-28 19:08:59 +0100

    ft_rec_dir function works with a list to sort before printing and some added tests. Two last tests don't work yet

--- source_files/list_files/ft_rec_dir.c
+++ source_files/list_files/ft_rec_dir.c
@@ -6,7 +6,7 @@
 /*   By: ivan-tey <ivan-tey@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/12/23 12:35:11 by ivan-tey       #+#    #+#                */
-/*   Updated: 2019/12/28 17:53:49 by lgutter       ########   odam.nl         */
+/*   Updated: 2019/12/28 19:07:08 by ivan-tey      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,9 @@ static void		ft_print_cur_dir(t_file_info *item)
 	ft_printf("\n%s:\n", directory);
 }
 
-static int		ft_check_rec(struct dirent *direntry, t_options options)
+static int		ft_check_rec(t_file_info *current, t_options options)
 {
-	if (direntry->d_type != DT_DIR)
+	if (!(S_ISDIR(current->lstats.st_mode)))
 	{
 		return (-1);
 	}
@@ -33,15 +33,15 @@ static int		ft_check_rec(struct dirent *direntry, t_options options)
 	{
 		return (-1);
 	}
-	else if ((options & e_opt_a) == 0 && (direntry->d_name[0] == '.'))
+	else if ((options & e_opt_a) == 0 && (current->name_pointer[0] == '.'))
 	{
 		return (-1);
 	}
-	else if (strcmp(direntry->d_name, ".") == 0)
+	else if (ft_strcmp(current->name_pointer, ".") == 0)
 	{
 		return (-1);
 	}
-	else if (strcmp(direntry->d_name, "..") == 0)
+	else if (ft_strcmp(current->name_pointer, "..") == 0)
 	{
 		return (-1);
 	}
@@ -51,14 +51,13 @@ static int		ft_check_rec(struct dirent *direntry, t_options options)
 	}
 }
 
-static int		ft_print_dir(char dirname[PATH_MAX + 1], t_options *options)
+static int		ft_print_dir(char dirname[PATH_MAX + 1], t_options *options,\
+							t_file_info **list_start)
 {
 	DIR				*dir;
 	struct dirent	*direntry;
-	t_file_info		*list_start;
 	char			file[PATH_MAX + 1];
 
-	list_start = NULL;
 	dir = opendir(dirname);
 	if (dir == NULL && ft_print_error(errno, dirname))
 		return (-1);
@@ -67,43 +66,42 @@ static int		ft_print_dir(char dirname[PATH_MAX + 1], t_options *options)
 	{
 		if (ft_path_expand(file, dirname, direntry->d_name, *options) == -1)
 			return (-1);
-		ft_stats_to_list(file, &list_start);
+		ft_stats_to_list(file, list_start);
 		direntry = readdir(dir);
 	}
 	closedir(dir);
-	ft_sort_list(&list_start, *options);
+	ft_sort_list(list_start, *options);
 	if ((*options & e_args) != 0)
-		ft_print_cur_dir(list_start);
+		ft_print_cur_dir(*list_start);
 	*options |= e_args;
-	ft_print_files_info(list_start, *options);
+	ft_print_files_info(*list_start, *options);
 	return (0);
 }
 
 void			ft_rec_dir(char dirname[PATH_MAX + 1], t_options options)
 {
 	DIR				*dirp;
-	struct dirent	*direntry;
+	t_file_info		*dir_list;
 	char			next_dir[PATH_MAX + 1];
 
+	dir_list = NULL;
 	dirp = opendir(dirname);
 	if (dirp == NULL)
 	{
 		ft_print_error(errno, dirname);
 		return ;
 	}
-	if (ft_print_dir(dirname, &options) == -1)
+	if (ft_print_dir(dirname, &options, &dir_list) == -1)
 		return ;
-	direntry = readdir(dirp);
-	while (direntry != NULL)
+	while (dir_list != NULL)
 	{
-		if (ft_check_rec(direntry, options) == 1)
+		if (ft_check_rec(dir_list, options) == 1)
 		{
-			if (ft_path_expand(next_dir, dirname, direntry->d_name,\
+			if (ft_path_expand(next_dir, dirname, dir_list->name_pointer,\
 								options) == -1)
 				return ;
 			ft_rec_dir(next_dir, options);
 		}
-		direntry = readdir(dirp);
+		dir_list = dir_list->next;
 	}
-	closedir(dirp);
 }
