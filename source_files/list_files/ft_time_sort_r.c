c095cd7b27a31dfe9f89c1025cef4ddc8f4d18a0
Liewe Gutter <lgutter@student.codam.nl>
2019-12-30 12:31:26 +0100

    improved time sort with nanoseconds

--- source_files/list_files/ft_time_sort_r.c
+++ source_files/list_files/ft_time_sort_r.c
@@ -6,7 +6,7 @@
 /*   By: lgutter <lgutter@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/12/22 13:25:14 by lgutter        #+#    #+#                */
-/*   Updated: 2019/12/22 14:19:03 by lgutter       ########   odam.nl         */
+/*   Updated: 2019/12/30 10:24:13 by lgutter       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,25 @@ static t_file_info	*ft_swap_start_list(t_file_info **list_start)
 	return (*list_start);
 }
 
+static int			ft_checktime(t_file_info *one, t_file_info *two)
+{
+	if (one->lstats.st_mtimespec.tv_sec <\
+			two->lstats.st_mtimespec.tv_sec)
+	{
+		return (1);
+	}
+	else if (one->lstats.st_mtimespec.tv_sec ==\
+			two->lstats.st_mtimespec.tv_sec)
+	{
+		if (one->lstats.st_mtimespec.tv_nsec <\
+			two->lstats.st_mtimespec.tv_nsec)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
+
 void				ft_time_sort_r(t_file_info **list_start)
 {
 	t_file_info	*current;
@@ -38,8 +57,7 @@ void				ft_time_sort_r(t_file_info **list_start)
 	future = current->next;
 	while (current->next != NULL)
 	{
-		if (future->lstats.st_mtimespec.tv_sec <\
-			current->lstats.st_mtimespec.tv_sec)
+		if (ft_checktime(future, current) == 1)
 		{
 			previous->next = future;
 			current->next = future->next;
