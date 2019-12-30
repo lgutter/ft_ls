ffbf140199451fd3194dfb2d12d8d4735d5ee0a2
Liewe Gutter <lgutter@student.codam.nl>
2019-12-30 17:14:10 +0100

    fixed tests

--- test_files/ft_stats_to_list_tests.c
+++ test_files/ft_stats_to_list_tests.c
@@ -6,7 +6,7 @@
 /*   By: lgutter <lgutter@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/12/18 10:47:50 by lgutter        #+#    #+#                */
-/*   Updated: 2019/12/29 10:58:02 by lgutter       ########   odam.nl         */
+/*   Updated: 2019/12/30 17:13:45 by lgutter       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ Test(ft_stats_to_list_tests, simple_existent_file_empty_list)
 
 	filenames[0] = "test";
 	filenames[1] = "ft_ls.h";
-	ret = ft_stats_to_list(filenames[1],  &list_start);
+	ret = ft_stats_to_list(filenames[1],  &list_start, options);
 
 	cr_assert_neq(ret, NULL);
 	cr_assert_str_eq(ret->path, filenames[1]);
@@ -44,7 +44,7 @@ Test(ft_stats_to_list_tests, simple_nonexistent_file_empty_list, .init = redirec
 
 	filenames[0] = "test";
 	filenames[1] = "this/file/does/not/exist";
-	ret = ft_stats_to_list(filenames[1],  &list_start);
+	ret = ft_stats_to_list(filenames[1],  &list_start, options);
 	fflush(stderr);
 
 	cr_assert_stderr_eq_str("ft_ls: this/file/does/not/exist: No such file or directory\n");
@@ -63,9 +63,9 @@ Test(ft_stats_to_list_tests, simple_existent_files_empty_list)
 	filenames[0] = "test";
 	filenames[1] = "ft_ls.h";
 	filenames[2] = "ft_printf";
-	ret = ft_stats_to_list(filenames[1],  &list_start);
+	ret = ft_stats_to_list(filenames[1],  &list_start, options);
 	cr_assert_neq(ret, NULL);
-	ret = ft_stats_to_list(filenames[2],  &list_start);
+	ret = ft_stats_to_list(filenames[2],  &list_start, options);
 	cr_assert_neq(ret, NULL);
 
 	cr_assert_neq(list_start, NULL);
@@ -91,11 +91,11 @@ Test(ft_stats_to_list_tests, simple_existent_3_files_empty_list)
 	filenames[1] = "ft_ls.h";
 	filenames[2] = "ft_printf";
 	filenames[3] = "end2endls.sh";
-	ret = ft_stats_to_list(filenames[1],  &list_start);
+	ret = ft_stats_to_list(filenames[1],  &list_start, options);
 	cr_assert_neq(ret, NULL);
-	ret = ft_stats_to_list(filenames[2],  &list_start);
+	ret = ft_stats_to_list(filenames[2],  &list_start, options);
 	cr_assert_neq(ret, NULL);
-	ret = ft_stats_to_list(filenames[3],  &list_start);
+	ret = ft_stats_to_list(filenames[3],  &list_start, options);
 	cr_assert_neq(ret, NULL);
 
 	cr_assert_neq(list_start, NULL);
@@ -119,7 +119,7 @@ Test(ft_stats_to_list_tests, link_file_empty_list)
 
 	filenames[0] = "test";
 	filenames[1] = "test_files/files_for_tests/link_to_ft_ls_h";
-	ret = ft_stats_to_list(filenames[1],  &list_start);
+	ret = ft_stats_to_list(filenames[1],  &list_start, options);
 	cr_assert_neq(ret, NULL);
 
 	cr_assert_neq(list_start, NULL);
@@ -132,16 +132,3 @@ Test(ft_stats_to_list_tests, link_file_empty_list)
 	cr_assert_eq(i, 1);
 }
 
-Test(ft_stats_to_list_tests, link_file_no_permission_empty_list)
-{
-	int argc = 2;
-	t_file_info *ret;
-	t_file_info *list_start = NULL;
-	t_options options = 0;
-	char **filenames = (char **)malloc(sizeof(char *) * argc);
-
-	filenames[0] = "test";
-	filenames[1] = "test_files/files_for_tests/bullshitnothere";
-	ret = ft_stats_to_list(filenames[1],  &list_start);
-	cr_assert_eq(ret, NULL);
-}
