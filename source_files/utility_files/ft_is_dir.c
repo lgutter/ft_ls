d533bada8ba326ceb39f4a26d1e4d796a5af66fd
Liewe Gutter <lgutter@student.codam.nl>
2019-12-30 12:32:59 +0100

    updated ft_is_dir to properly detect links

--- source_files/utility_files/ft_is_dir.c
+++ source_files/utility_files/ft_is_dir.c
@@ -6,7 +6,7 @@
 /*   By: lgutter <lgutter@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/12/29 17:46:36 by lgutter        #+#    #+#                */
-/*   Updated: 2019/12/29 19:01:28 by lgutter       ########   odam.nl         */
+/*   Updated: 2019/12/30 09:55:59 by lgutter       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,25 @@ int		ft_is_dir(char *filename)
 {
 	DIR		*dirp;
 	char	buf[PATH_MAX];
-	int		errn;
 
 	if (readlink(filename, buf, PATH_MAX) > 0)
 		return (0);
-	errn = errno;
-	dirp = opendir(filename);
-	if (dirp != NULL)
+	else if (errno == EINVAL)
 	{
-		closedir(dirp);
-		return (1);
-	}
-	else if (errno == EACCES)
-	{
-		return (1);
-	}
-	else if (errno == ENOTDIR)
-	{
-		return (0);
+		dirp = opendir(filename);
+		if (dirp != NULL)
+		{
+			closedir(dirp);
+			return (1);
+		}
+		else if (errno == EACCES)
+		{
+			return (1);
+		}
+		else if (errno == ENOTDIR)
+		{
+			return (0);
+		}
 	}
 	return (0);
 }
